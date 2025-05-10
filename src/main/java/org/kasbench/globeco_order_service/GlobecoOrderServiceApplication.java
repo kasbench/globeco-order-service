ba0d20939c74761894c159187f456cd34d0c8bf9
Noah Krieger <noahkrieger@gmail.com>
2025-05-10 15:26:21 -0400

    Initialized Spring

---
+++ src/main/java/org/kasbench/globeco_order_service/GlobecoOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package org.kasbench.globeco_order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobecoOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobecoOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kasbench/globeco_order_service/GlobecoOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kasbench.globeco_order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobecoOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
