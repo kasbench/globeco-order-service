5e90a705496b20ecf5d128dbe9439cb04b622d1f
Noah Krieger <noahkrieger@gmail.com>
2025-11-04 21:22:19 -0500

    froze docker tags

--- kbuild.sh
+++ kbuild.sh
@@ -1,3 +1,6 @@
+docker buildx build --platform linux/amd64,linux/arm64 \
+-t kasbench/globeco-order-service:latest \
+-t kasbench/globeco-order-service:1.0.0 \
+--push .
 kubectl delete -f k8s/deployment.yaml
-docker buildx build --platform linux/amd64,linux/arm64 -t kasbench/globeco-order-service:latest --push .
 kubectl apply -f k8s/deployment.yaml
